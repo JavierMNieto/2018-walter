018bf3adbc5b8d85e594aa1b946a4069a24bf361
ethanhershberger <ethanhershberger@localhost>
2018-08-01 20:55:06 -0500

    this is the program for the arm and the roller

---
+++ src/main/java/subsystems/Arm.java
@@ -0,0 +1,36 @@
+package subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Arm extends Subsystem {
+	private static final double UP_SPEED = 1;
+	private static final double DOWN_SPEED = 1;
+	private WPI_TalonSRX armMotor;
+	
+	public Arm() {
+		armMotor = new WPI_TalonSRX(0); //FIXME put real value
+	}
+	
+	@Override
+	protected void initDefaultCommand() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void up() {
+		armMotor.set(UP_SPEED);
+	}
+		
+	public void down() {
+		armMotor.set(DOWN_SPEED);
+	}
+			
+	public void stop() {
+		armMotor.set(0);
+	}
+		
+}
+
+
