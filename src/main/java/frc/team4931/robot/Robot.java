928787e36a280a834d046bfe861ed1feb6459ddf
DJWICK2000 <dj_bakugan@Charter.net>
2018-08-08 20:08:33 -0500

    Added onto the Robot class

--- src/main/java/frc/team4931/robot/Robot.java
+++ src/main/java/frc/team4931/robot/Robot.java
@@ -1,10 +1,25 @@
 package frc.team4931.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
+import subsystem.Drivetrain;
+import subsystems.Arm;
+import subsystems.Roller;
 
 public class Robot extends IterativeRobot {
+  
+  public static OperatorInput operatorInput;
+  public static Drivetrain drivetrain;
+  public static Arm arm;
+  public static Roller roller;
+  
   @Override
   public void robotInit() {
+    drivetrain = new Drivetrain();
+    operatorInput = new OperatorInput();
+    
+    arm = new Arm();
+    roller = new roller();
+    
     System.out.println("Hello World! Beep Boop!");
   }
 }
