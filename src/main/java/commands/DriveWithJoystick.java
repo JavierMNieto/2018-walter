29ebf33c1ba767109c4fd0af4feedf91e9eb1846
robbiereiheld <robbiereiheld@localhost>
2018-08-15 21:28:34 -0400

    inverted the joy stick and set the variables of the drive train and the joy stick
    finished drive with joy stick

--- src/main/java/commands/DriveWithJoystick.java
+++ src/main/java/commands/DriveWithJoystick.java
@@ -1,11 +1,20 @@
 package commands;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.command.Command;
+import frc.team4931.robot.Robot;
+import subsystems.Drivetrain;
 
 public class DriveWithJoystick extends Command {
-  
+  private Joystick joystick;
+  private Drivetrain drivetrain;
+  public DriveWithJoystick() {
+	  joystick = Robot.operatorInput.getJoystick();
+	  drivetrain = Robot.drivetrain;
+  }
   @Override
-  protected void execute() {
+  protected void execute() { 
+    drivetrain.arcadeDrive(joystick.getX() * -1, joystick.getZ(), 1 - ((joystick.getThrottle() + 1) / 2));
     
   }
   
