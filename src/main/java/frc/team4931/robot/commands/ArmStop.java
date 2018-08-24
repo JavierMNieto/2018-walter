4c62d6ad79b5a0e060e5bf37043043926d612bd0
DJWICK2000 <dj_bakugan@Charter.net>
2018-08-23 19:59:59 -0500

    Created the ArmDown and the ArmStop command classes

---
+++ src/main/java/frc/team4931/robot/commands/ArmStop.java
@@ -0,0 +1,19 @@
+package frc.team4931.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.team4931.robot.Robot;
+
+public class ArmStop extends Command {
+
+    public ArmStop (){
+        setInterruptible(false);
+    }
+
+    protected boolean isFinished(){
+        return true;
+    }
+
+    protected void initialize(){
+        Robot.arm.stop();
+    }
+}
