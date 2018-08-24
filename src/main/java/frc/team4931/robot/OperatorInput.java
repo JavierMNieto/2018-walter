94025c4d2555dc08de855e0bb9be6ce67501a056
Ethan Hershberger <skylanderguy117@gmail.com>
2018-08-23 19:16:25 -0500

    gave joystick button commands to RollerSuck and Roller Spit

--- src/main/java/frc/team4931/robot/OperatorInput.java
+++ src/main/java/frc/team4931/robot/OperatorInput.java
@@ -1,17 +1,28 @@
 package frc.team4931.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import frc.team4931.robot.commands.RollerSpit;
+import frc.team4931.robot.commands.RollerSuck;
 
 public class OperatorInput {
   private Joystick joystick;
   
   public OperatorInput() {
     joystick = makeJoystick();
+   
   }
   
   private Joystick makeJoystick() {
     Joystick stick = new Joystick(0); //FIXME value 
     //TODO bind buttons
+    
+    Button rollerSpit = new JoystickButton(stick, 1);
+    Button rollerSuck = new JoystickButton(stick, 2);
+    
+    rollerSpit.whileHeld(new RollerSpit());
+    rollerSuck.whileHeld(new RollerSuck());
     return stick;
   }
   
