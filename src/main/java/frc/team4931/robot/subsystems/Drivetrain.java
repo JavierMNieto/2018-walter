3a02900779a160f4f2e3468dce29e46de0390416
DJWICK2000 <dj_bakugan@Charter.net>
2018-09-06 20:57:13 -0500

    Reconfigured motors from talons to victors

--- src/main/java/frc/team4931/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/team4931/robot/subsystems/Drivetrain.java
@@ -1,9 +1,8 @@
 package frc.team4931.robot.subsystems;
 
 
-
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.VictorSP;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -12,16 +11,16 @@ import frc.team4931.robot.commands.DriveWithJoystick;
 
 public class Drivetrain extends Subsystem {
 
-  private WPI_TalonSRX leftFront, rightFront, leftBack, rightBack;
+  private VictorSP leftFront, rightFront, leftBack, rightBack;
   private SpeedControllerGroup left, right;
   private DifferentialDrive drive;
 
   public Drivetrain() {
 
-    leftFront = new WPI_TalonSRX(RobotMap.LEFT_FRONT); // FIXME add real values
-    rightFront = new WPI_TalonSRX(RobotMap.RIGHT_FRONT);
-    leftBack = new WPI_TalonSRX(RobotMap.LEFT_BACK);
-    rightBack = new WPI_TalonSRX(RobotMap.RIGHT_BACK);
+    leftFront = new VictorSP(RobotMap.LEFT_FRONT); // FIXME add real values
+    rightFront = new VictorSP(RobotMap.RIGHT_FRONT);
+    leftBack = new VictorSP(RobotMap.LEFT_BACK);
+    rightBack = new VictorSP(RobotMap.RIGHT_BACK);
 
     left = new SpeedControllerGroup(leftFront, leftBack);
     right = new SpeedControllerGroup(rightFront, rightBack);
